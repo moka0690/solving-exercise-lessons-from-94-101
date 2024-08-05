428f1b907d321faa6f7b53668b39239994d7d2ed
moka0690 <madradz39@gmail.com>
2024-08-05 20:11:39 +0100

    Exercise solution  3

---
+++ ex3.js
@@ -0,0 +1,18 @@
+const paragraph = document.querySelector('p');
+    paragraph.remove();
+
+ const divElement = document.querySelector('.our-element');
+ const divStart = document.createElement('div');
+ divStart.className = 'start';
+ divStart.title = 'Start Element';
+ divStart.textContent = 'Start';
+ divStart.setAttribute  ('data-value', 'Start')
+ divElement.before(divStart);
+
+ const divEnd = document.createElement('div');
+ divEnd.className = 'end';
+ divEnd.title = 'End Element';
+ divEnd.textContent = 'End';
+ divEnd.setAttribute  ('data-value', 'End')
+  divElement.after(divEnd);
+ 
